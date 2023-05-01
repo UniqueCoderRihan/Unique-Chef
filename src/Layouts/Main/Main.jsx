69917de5d572e8fd3da17c4a9d5467526e1108f7
Rihanul Islam <rihanthedev@gmail.com>
2023-05-01 23:32:10 +0600

    Footer done

--- src/Layouts/Main/Main.jsx
+++ src/Layouts/Main/Main.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import Nabvar from '../../Components/Nabvar/Nabvar';
 import { Outlet } from 'react-router-dom';
+import Footer from '../../Components/Footer/Footer';
 
 const Main = () => {
     return (
         <div>
             <Nabvar></Nabvar>
             <Outlet></Outlet>
+            <Footer></Footer>
         </div>
     );
 };
