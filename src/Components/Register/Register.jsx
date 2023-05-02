d4b8b7051728f72c1d7b1ec3268ccaf56692a308
Rihanul Islam <rihanthedev@gmail.com>
2023-05-02 19:28:43 +0600

    Registion error solved

--- src/Components/Register/Register.jsx
+++ src/Components/Register/Register.jsx
@@ -2,6 +2,14 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Register = () => {
+    const handleSubmit = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const fullName = form.fullName.value;
+        const password = form.password.value;
+        console.log(fullName,email,password);
+    }
     return (
         <div className="hero min-h-screen bg-base-200">
             <div className="hero-content flex-col lg:flex-row-reverse">
@@ -10,7 +18,7 @@ const Register = () => {
                     <p className="py-6">If You get Our New Updated And More Information Then You Must be Able our Membership</p>
                 </div>
                 <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                    <div className="card-body">
+                    <form onSubmit={handleSubmit} className="card-body">
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Enter Your Full Name</span>
@@ -27,26 +35,26 @@ const Register = () => {
                             <label className="label">
                                 <span className="label-text">Email</span>
                             </label>
-                            <input type="text" placeholder="email" className="input input-bordered" />
+                            <input type="email" name='email' placeholder="email" className="input input-bordered" />
                         </div>
                         <div className="form-control">
                             <label className="label">
                                 <span className="label-text">Password</span>
                             </label>
-                            <input type="text" placeholder="password" className="input input-bordered" />
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
                             <label className="label">
                                 <p><small>Alredy have an Account? <Link className="label-text-alt link link-hover" to='/login'>Login</Link> </small></p>
                             </label>
                         </div>
                         <div className="form-control">
-                            <button className="btn btn-primary">Register Now</button>
+                            <button type='submit' className="btn btn-primary">Register Now</button>
                         </div>
                         <div>
                             <p className='text-center text-xl'>Or</p>
                             <button className='btn  btn-outline btn-primary w-full my-2'>Continue With Google</button>
                             <button className='btn  btn-outline btn-warning w-full'>Continue With Google</button>
                         </div>
-                    </div>
+                    </form>
                 </div>
             </div>
         </div>
