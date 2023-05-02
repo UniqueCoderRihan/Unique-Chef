1dcc9e6b43760299feaa43aa1aa565091efcb4f9
Rihanul Islam <rihanthedev@gmail.com>
2023-05-03 00:31:38 +0600

    Update Nabvar

--- src/Components/Nabvar/Nabvar.jsx
+++ src/Components/Nabvar/Nabvar.jsx
@@ -21,7 +21,6 @@ const Nabvar = () => {
                 <ul className="menu menu-horizontal text-3xl px-1 text-orange-200">
                     <Link to='/'>Home</Link>
                     <Link className='mx-4' to='/blog'>Blogs</Link>
-                    <Link  to='/about'>About Us</Link>
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full className='mx-2'">
                             <img src="https://scontent.fcgp32-1.fna.fbcdn.net/v/t39.30808-6/343562529_1850575275326196_3198939309385702606_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGrtHVNj9AMag3gkBUFg6CJy46jFL_7pdDLjqMUv_ul0L2CYjCosXoa5BnsYfAcnga7047-kIA_eIeL0hYMF8cC&_nc_ohc=xGL05SUiNgcAX_JfGZ7&_nc_ht=scontent.fcgp32-1.fna&oh=00_AfB7eV56NXF9KZVPTudAzeX19Z2B8qQXcLaW6RC-VGx6xQ&oe=64544442" />
