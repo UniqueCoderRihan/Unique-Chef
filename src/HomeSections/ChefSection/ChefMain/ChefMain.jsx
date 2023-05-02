baad4655dde61c236c2709b0c73c3a83e5338d1c
Rihanul Islam <rihanthedev@gmail.com>
2023-05-03 00:09:11 +0600

    Chef Section V.001

---
+++ src/HomeSections/ChefSection/ChefMain/ChefMain.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const ChefMain = () => {
+    return (
+        <div>
+            <h1>Hi This is Chef Section</h1>
+        </div>
+    );
+};
+
+export default ChefMain;
--- src/Layouts/Home/Home.jsx
+++ src/Layouts/Home/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import Banner from '../../HomeSections/Banner/Banner';
+import ChefMain from '../../HomeSections/ChefSection/ChefMain/ChefMain';
 
 const Home = () => {
     return (
         <div>
             <Banner></Banner>
+            <ChefMain></ChefMain>
         </div>
     );
 };
