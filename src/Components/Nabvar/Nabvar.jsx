30dccac6ba59b6e726a6e77480c71bd4f25b346b
Rihanul Islam <rihanthedev@gmail.com>
2023-05-04 01:08:32 +0600

    Active Routes done

--- src/Components/Nabvar/Nabvar.jsx
+++ src/Components/Nabvar/Nabvar.jsx
@@ -1,5 +1,5 @@
 import React, { useContext } from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import { AuthContex } from '../../AuthProvider/AuthProvider';
 
 const Nabvar = () => {
@@ -21,8 +21,26 @@ const Nabvar = () => {
             </div>
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal text-3xl px-1 text-orange-200">
-                    <Link to='/'>Home</Link>
-                    <Link className='mx-4' to='/blog'>Blogs</Link>
+                    
+                    <NavLink
+                    to='/'
+                    style={({ isActive, isPending }) => {
+                        return {
+                          fontWeight: isActive ? "bold" : "",
+                          color: isPending ? "red" : "black",
+                        };
+                      }}
+                    >Home</NavLink>
+                    <NavLink
+                    to='/blog'
+                    style={({ isActive, isPending }) => {
+                        return {
+                          fontWeight: isActive ? "bold" : "",
+                          color: isPending ? "red" : "black",
+                        };
+                      }}
+                    > <span className='mx-4'>Blogs</span> </NavLink>
+                    
                     <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
                         <div className="w-10 rounded-full className='mx-2'">
                             <img src="https://scontent.fcgp32-1.fna.fbcdn.net/v/t39.30808-6/343562529_1850575275326196_3198939309385702606_n.jpg?_nc_cat=103&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGrtHVNj9AMag3gkBUFg6CJy46jFL_7pdDLjqMUv_ul0L2CYjCosXoa5BnsYfAcnga7047-kIA_eIeL0hYMF8cC&_nc_ohc=xGL05SUiNgcAX_JfGZ7&_nc_ht=scontent.fcgp32-1.fna&oh=00_AfB7eV56NXF9KZVPTudAzeX19Z2B8qQXcLaW6RC-VGx6xQ&oe=64544442" />
