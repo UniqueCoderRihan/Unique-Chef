caff3464447f58fb1472810336236d4b391e5735
Rihanul Islam <rihanthedev@gmail.com>
2023-05-04 12:53:06 +0600

    link to layout

--- src/Components/ChefCard/ChefCard.jsx
+++ src/Components/ChefCard/ChefCard.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import {FaHeart,FaLongArrowAltRight,FaRegBookmark} from 'react-icons/fa';
+import { Link } from 'react-router-dom';
 
 const ChefCard = ({chef}) => {
     const {name,age,id,numberOfRecipes,yearsOfExperience,description,rating,imageUrl,likes} = chef;
+    
     return (
         <div className="card w-96 glass">
                 <figure><img className='w-50' loading="lazy" src={imageUrl} alt="Man or Women pick!" /></figure>
@@ -17,7 +19,7 @@ const ChefCard = ({chef}) => {
                     
                     <div className="card-actions">
                         <p className='my-auto font-semibold'>Likes: {likes} </p>
-                        <button className="btn btn-primary" > <span className='mr-2'>View Recipes</span> <FaLongArrowAltRight></FaLongArrowAltRight> </button>
+                        <button onClick={`/recipe/${id}`} className="btn btn-primary" > <span className='mr-2'> <Link to={`/recipe/${id}`}>View Recipes</Link> </span> <FaLongArrowAltRight></FaLongArrowAltRight> </button>
                     </div>
                 </div>
             </div>
